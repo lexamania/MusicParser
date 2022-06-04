61fac7785a187a8dd9984eaa95ea0e07aa586d17
Lex <torbanowmain@gmail.com>
2022-06-04 23:19:17 +0300

    project init

---
+++ MusicParser/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MusicParser
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
